c520f1128ffacd3feebab0126257412b5c6e753f
Eugene Francisco <eugjavfra01@gmail.com>
2025-03-18 15:43:54 -0700

    added basic experience trace testing to the tests file

--- src/tests.py
+++ src/tests.py
@@ -1,6 +1,6 @@
-from utils import produce_trace, step_minibatch
+from utils import produce_trace, step_minibatch, make_experience_trace
 from distributions import Distribution, NegatedParetoDistribution
-from function_approx import SimpleNNApprox
+from function_approx import SimpleNNApprox, DummyQApprox
 from typing import Callable, Iterable, Mapping
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,21 +8,25 @@ from abc import ABC, abstractmethod
     
 config = {
     'start_price': 100,
-    'driftRate': 0.000,
+    'start_time': 100,
+    'strike_price': 100,
+    'driftRate': 0.005,
     'timeGap': 1
 }
 
 distribution = NegatedParetoDistribution(scale = 0.03, alpha = 2.0)
 
-path = produce_trace(config = config, distribution = distribution)
-price_list_path = [next(path) for _ in range(100)]
+#path = produce_trace(config = config, distribution = distribution)
+#price_list_path = [next(path) for _ in range(100)]
 #plt.plot(price_list_path)
 
-QFunctionApprox = SimpleNNApprox(learning_rate=0.01)
-print(QFunctionApprox.model)
+QFunctionApprox = DummyQApprox()
+experience_trace = make_experience_trace(config, distribution, QFunctionApprox)
+price_list = [tup[0].price for tup in experience_trace]
+plt.plot(price_list)
+plt.show()
+
 
 # next_prices = step_minibatch(config = config, distribution = distribution, batch_size=100)
 # plt.hist(next_prices, bins=100, density=True)
 # plt.xlabel('Next Price')
-
-#plt.show()
